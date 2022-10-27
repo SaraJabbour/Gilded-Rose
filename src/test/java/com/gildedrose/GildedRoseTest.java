af7784b7a08f69454bc9b9429c8190eab780307d
Zeva <89845369+zeva02@users.noreply.github.com>
2022-10-27 11:35:13 +0300

    Test unit

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,12 +8,12 @@ class GildedRoseTest {
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("backsTage Passes to F1 paddock", 1, 9),
-            new Item("cONjuRed mellow cake",3,10), };
+            new Item("cONjuRed mellow cake",3,10), new Item("Aged brie",5,48), new Item("Moonpie",2,5) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
         //Testing values
-        assertEquals(12, app.items[0].quality);
+        assertEquals(0, app.items[0].quality);
         assertEquals(0, app.items[0].sellIn);
         assertEquals(Item.Type.Backstage, app.items[0].type);
 
@@ -21,16 +21,24 @@ class GildedRoseTest {
         assertEquals(2, app.items[1].sellIn);
         assertEquals(Item.Type.Conjured, app.items[1].type);
 
+
         app.updateQuality();
 
         assertEquals(0, app.items[0].quality);
-        assertEquals(0, app.items[0].sellIn);
+        assertEquals(-1, app.items[0].sellIn);
         assertEquals(Item.Type.Backstage, app.items[0].type);
 
         assertEquals(6, app.items[1].quality);
         assertEquals(1, app.items[1].sellIn);
         assertEquals(Item.Type.Conjured, app.items[1].type);
 
+        assertEquals(50, app.items[2].quality);
+        assertEquals(3, app.items[2].sellIn);
+        assertEquals(Item.Type.AgedBrie, app.items[2].type);
+
+        assertEquals(2, app.items[3].quality);
+        assertEquals(0, app.items[3].sellIn);
+        assertEquals(Item.Type.Regular, app.items[3].type);
     }
 
 }
