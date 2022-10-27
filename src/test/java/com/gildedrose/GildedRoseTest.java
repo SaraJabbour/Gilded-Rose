970425a5fb4c4501ff51b63ed89073d11ee9f35f
Zeva <89845369+zeva02@users.noreply.github.com>
2022-10-27 13:12:52 +0300

    Unit test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,8 @@ class GildedRoseTest {
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("backsTage Passes to F1 paddock", 1, 9),
-            new Item("cONjuRed mellow cake",3,10), new Item("Aged brie",5,48), new Item("Moonpie",2,5) };
+            new Item("cONjuRed mellow cake",3,10), new Item("Aged brie",5,48),
+            new Item("Moonpie",2,5), new Item("Sulfuras, Hand of Thanos",-3,80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
@@ -39,6 +40,11 @@ class GildedRoseTest {
         assertEquals(2, app.items[3].quality);
         assertEquals(0, app.items[3].sellIn);
         assertEquals(Item.Type.Regular, app.items[3].type);
+
+        assertEquals(80, app.items[4].quality);
+        assertEquals(-3, app.items[4].sellIn);
+        assertEquals(Item.Type.Sulfuras, app.items[4].type);
+
     }
 
 }
