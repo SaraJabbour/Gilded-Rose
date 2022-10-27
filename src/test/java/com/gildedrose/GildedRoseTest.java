2b58aab252cd210433d72356054221af97fc2463
Zeva <89845369+zeva02@users.noreply.github.com>
2022-10-27 11:20:26 +0300

    Add test unit

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,20 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("backsTage Passes to F1 paddock", 2, 6),
-            new Item("cONjuRed mellow cake",3,10) };
+        Item[] items = new Item[] { new Item("backsTage Passes to F1 paddock", 1, 9),
+            new Item("cONjuRed mellow cake",3,10), };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
         //Testing values
-        assertEquals(9, app.items[0].quality);
-        assertEquals(1, app.items[0].sellIn);
+        assertEquals(12, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
         assertEquals(Item.Type.Backstage, app.items[0].type);
 
         assertEquals(8, app.items[1].quality);
         assertEquals(2, app.items[1].sellIn);
         assertEquals(Item.Type.Conjured, app.items[1].type);
 
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(Item.Type.Backstage, app.items[0].type);
+
+        assertEquals(6, app.items[1].quality);
+        assertEquals(1, app.items[1].sellIn);
+        assertEquals(Item.Type.Conjured, app.items[1].type);
 
     }
 
